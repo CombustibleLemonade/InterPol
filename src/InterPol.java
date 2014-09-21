78d6fdc085a2022117b22b21b8bc5340dc44a106
Jan Heemstra <heemstrajan@gmail.com>
2014-09-21 18:44:43 +0200

    Made the project work, and added "Hello World!"

--- src/InterPol.java
+++ src/InterPol.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
 
 public class InterPol {
-
+    Scanner scanner = new Scanner(System.in);
+    
+    static public void main(String[] Args) {
+        System.out.println("Hello World!");
+    }
 }
