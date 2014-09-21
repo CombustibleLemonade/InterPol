b8efc157ca4985ba54d429a0caac016ffa8cb4a0
Jan Heemstra <heemstrajan@gmail.com>
2014-09-21 19:23:14 +0200

    Adding program outline

--- src/InterPol.java
+++ src/InterPol.java
@@ -1,9 +1,32 @@
+/**
+ * @author Olof Morra
+ * @author Jan Heemstra
+ * Homework assignment, with comments for explanation.
+ */
+
 import java.util.Scanner;
 
 public class InterPol {
-    Scanner scanner = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);//Add scanner object
+    
+    void queryFactors() {
+        
+    }
+    
+    void queryInterval() {
+        
+    }
+    
+    void query() {
+        queryFactors();
+        queryInterval();
+    }
+    
+    void start() {
+        query();
+    }
     
     static public void main(String[] Args) {
-        System.out.println("Hello World!");
+        new InterPol().start();
     }
 }
